4446d92d1ecf541de39aa6edf2d75ed7854387d2
shan64 <methikarshantanu@gmail.com>
2023-02-11 17:31:11 +0530

    Add virtual host and host local html in WebVIew2

--- InteropBetweenWebView2AndCSharp/MainPage.xaml.cs
+++ InteropBetweenWebView2AndCSharp/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml;
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace InteropBetweenWebView2AndCSharp
@@ -11,6 +13,17 @@ namespace InteropBetweenWebView2AndCSharp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void MainPageLoaded(object sender, RoutedEventArgs e)
+        {
+            await WebView2.EnsureCoreWebView2Async();
+
+            WebView2.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                "webview2.uwp.example",
+                "Assets/html",
+                CoreWebView2HostResourceAccessKind.Allow);
+            WebView2.Source = new Uri("http://webview2.uwp.example/index.html");
         }
 
         private void LoadImageButtonClicked(object sender, RoutedEventArgs e)
